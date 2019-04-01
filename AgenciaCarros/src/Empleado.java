6d5a150b4ee2a791c02d0b0f93ad9080128980dd
Carlos Lopez <a01633683@itesm.mx>
2019-04-01 09:45:41 -0600

    Trabajo en clase Empleado
    +Se agregaron atributos primordiales

--- AgenciaCarros/src/Empleado.java
+++ AgenciaCarros/src/Empleado.java
@@ -1,4 +1,19 @@
+import java.awt.Image;
 
 public class Empleado {
 
+	private String primerNombre,
+				   segundoNombre,
+				   apellidoPaterno,
+				   apellidoMaterno,
+				   matricula,
+				   cargo,
+				   rfc;
+	private int entradaALaEmpresa;
+	private Image foto;
+	
+	public Empleado(String primerNombre, String appelidoPaterno, String apellidoMaterno, String rfc, int entradaALaEmpresa) {
+		
+	}
+	
 }
